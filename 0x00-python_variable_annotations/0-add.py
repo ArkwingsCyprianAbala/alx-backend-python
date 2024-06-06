7712d9fc7eaa9805390abaceb8a7256571fd3fdc
Cyprian Abala <arkwingscyprianabala@gmail.com>
2024-06-07 01:28:02 +0300

    ALX Backend

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Type-annotated function add that takes a float a and a float b as arguments  and returns their sum as a float."""
+"""Type-annotated function add that takes a float b and return a float."""
+
+
 def add(a: float, b: float) -> float:
     """Add two floats and return a float."""
     return a + b
