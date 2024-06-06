35ae28e90c916802800581915e2532a715d799fa
Cyprian Abala <arkwingscyprianabala@gmail.com>
2024-06-07 01:53:48 +0300

    ALX Backend

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
-"""
-Type-annotated function type_check
-that takes an element and returns its type.
-"""
-from typing import Union, Any, Tuple, List, TypeVar
-T = TypeVar('T')
+"""Defines Type checking"""
+from typing import List, Tuple
 
 
-def type_check(element: Any) -> Tuple[Union[T, None], Union[T, None]]:
-    """Return tuple of element and its type."""
-    return (element, type(element))
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Type Checking Exercise"""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
